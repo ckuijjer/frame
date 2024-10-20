14a59a8667586f74e98848738cffc524258cf021
Casper Kuijjer <casper@kuijjer.com>
2024-10-20 16:45:29 +0200

    suffix instead of prefix in filename (easier to delete images)

--- utils.py
+++ utils.py
@@ -6,14 +6,14 @@ def snake_case(text: str) -> str:
     """Converts a given string to snake_case."""
     return re.sub(r'[\W_]+', '_', text).strip().lower()
 
-def get_filename(title: str, directory: str = "images", prefix: str = "") -> str:
+def get_filename(title: str, directory: str = "images", suffix: str = "") -> str:
     """
-    Generate a filename based on the current date and the article title, with an optional prefix, and store it in the images directory.
+    Generate a filename based on the current date and the article title, with an optional suffix, and store it in the images directory.
     
     Args:
         title (str): The article title.
         directory (str): The directory where the images will be stored. Defaults to 'images'.
-        prefix (str): Optional prefix to add to the filename (e.g., "resized_", "with_text_").
+        suffix (str): Optional suffix to add to the filename (e.g., "_resized", "_with_text").
     
     Returns:
         str: The full path of the generated filename.
@@ -23,5 +23,5 @@ def get_filename(title: str, directory: str = "images", prefix: str = "") -> str
 
     current_date = datetime.now().strftime("%Y-%m-%d")
     snake_case_title = snake_case(title)
-    filename = f"{prefix}{current_date}_{snake_case_title}.png"
+    filename = f"{current_date}_{snake_case_title}{suffix}.png"
     return os.path.join(directory, filename)
